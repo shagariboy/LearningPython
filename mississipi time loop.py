09bbe7aa7fea445a4bea2431eec8ec62429ce399
Nnamdi Ezeh <nnamdiezeh0@gmail.com>
2021-10-28 17:45:19 +0100

    This my first commit!

---
+++ mississipi time loop.py
@@ -0,0 +1,20 @@
+#import time
+
+# Write a for loop that counts to five.
+
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    # Body of the loop - use: time.sleep(1)
+
+#for i in range(1,6):
+#    print(i, "Mississipi")
+#    time.sleep(1)   
+#    print("Ready or not, here I come!")
+import time
+i = [1,2,3,4,5]
+for step in i:
+    time.sleep(1)
+    print(str(step),"Mississippi")
+time.sleep(1)
+print("Ready or not, here I come!")    
+
+# Write a print function with the final message.
