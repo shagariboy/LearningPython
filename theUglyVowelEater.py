13d4044d66536fffc9d64cb26ad21cb93aa3da8c
Nnamdi Ezeh <nnamdiezeh0@gmail.com>
2021-10-31 17:08:06 +0100

    The ugly vowel eater

---
+++ theUglyVowelEater.py
@@ -0,0 +1,21 @@
+# Prompt the user to enter a word
+# and assign it to the user_word variable.
+user_word = input("Enter a word: ")
+
+user_word = user_word.upper()
+for letter in user_word:
+    if letter == "A":
+        continue
+    if letter == "E":
+        continue
+    if letter == "I":
+        continue
+    if letter == "O":
+        continue
+    if letter == "U":
+        continue
+
+    print(letter)
+        
+    
+    # Complete the body of the for loop.
